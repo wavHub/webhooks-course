d9d8473c0c42bab2b88fa105ae5fb5a7b45fe656
wavHub <dev.apps@proton.me>
2025-11-09 06:46:42 +0700

    feat: Enhance Discord webhook messages for repository actions and update package-lock.json

--- code/netlify-discorder/functions/discorder/discorder.js
+++ code/netlify-discorder/functions/discorder/discorder.js
@@ -1,14 +1,29 @@
 const axios = require("axios").default;
-
+//This is an aync method and can us await. The discorder used axios make a request and a response
+// pulling off the bodey with "req.body" and "then" pattern. 
+// In Netlify we will use "try catch" pattern to handle errors.
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
+    console.log(event.body);
     const body = JSON.parse(event.body);
+    console.log(body);
     const username = body.sender.login;
     const avatarUrl = body.sender.avatar_url;
     const repoName = body.repository.name;
+    const action = body.action;
+
+    let content;
+    if (action === "created") {
+      content = `Look who just ⭐️ ${repoName}!\nThanks ${username}! :rocket:!`;
+    } else if (action === "deleted") {
+      content = `Oh no! ${username} unstarred ${repoName} 😢`;
+    } else {
+      content = `${username} performed action: ${action} on ${repoName}`;
+    }
+
     const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-      content: `:taco: :taco: :taco: ${username} just starred ${repoName}! :rocket: :muscle: :tada: :taco:`,
+      content: content,
       embeds: [
         {
           image: {
